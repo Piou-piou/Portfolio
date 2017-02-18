f5d10fca941f36bdec465748e935cfc1d29c1ad8
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-02-18 18:13:42 +0100

    change arbo of the view

---
+++ app/views/template/js/portfolio/stop_lien.js
@@ -0,0 +1,43 @@
+jQuery(document).ready(function() {
+
+    jQuery(".card__content").css({"display": "none"});
+
+    jQuery(".suite").click(function(e) {
+        e.preventDefault();
+
+        jQuery(".card__content").css({"display": "block"});
+    
+        jQuery("body").css("overflow-y", "hidden");
+
+        jQuery(".masque").css({"z-index":1});
+        jQuery(".img-presentation").css({
+            "display":"block",
+            "opacity": "100"
+        });
+    });
+
+    jQuery(".projet img").click(function(e) {
+        e.preventDefault();
+
+        jQuery(".card__content").css({"display": "block"});
+    
+        jQuery("body").css("overflow-y", "hidden");
+
+        jQuery(".masque").css({"z-index":1});
+        jQuery(".img-presentation").css({
+            "display":"block",
+            "opacity": "100"
+        });
+    });
+
+    jQuery(".card__btn-close").click(function() {
+        jQuery(".masque").css({"z-index":20});
+    
+            jQuery("body").css("overflow-y", "auto");
+
+        jQuery(".img-presentation").delay(30000000).css({
+            "opacity": "0",
+            "transition": "4s"
+        });
+    })
+});
